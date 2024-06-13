20bb6fa2cfacedcb1fec46c1cd974b546492d2ff
alexeylitvinov <alextwin1976@gmail.com>
2024-06-13 18:11:13 +0200

    some add

--- users/management/commands/superu.py
+++ users/management/commands/superu.py
@@ -1,13 +1,19 @@
+import os
+
 from django.core.management import BaseCommand
+from dotenv import load_dotenv
 
 from users.models import User
+
+load_dotenv()
 
 
 class Command(BaseCommand):
     """Кастомная команда создания суперпользователя"""
+
     def handle(self, *args, **options):
-        user = User.objects.create(email='admin@admin.com')
-        user.set_password('admin')
+        user = User.objects.create(email=os.getenv('S_EMAIL'))
+        user.set_password(os.getenv('S_PASSWORD'))
         user.is_active = True
         user.is_staff = True
         user.is_superuser = True
